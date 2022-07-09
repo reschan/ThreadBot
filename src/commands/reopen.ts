71c8f8874cb88c3b2d080ac581e31a7e46881c85
Or Gold <ortom345minecraft@gmail.com>
2022-07-10 00:15:56 +0300

    Removed unused command

--- src/commands/reopen.ts
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
-import { CommandInteraction } from 'discord.js';
-import { bug_channel } from '../config.json'
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('reopen').setDescription('Re-open a closed issue')
-}
