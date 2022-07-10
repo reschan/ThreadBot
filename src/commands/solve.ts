b5abc5ceb8248f1b3a23738e2c4e762d515e4b10
reschan <h>
2022-07-10 22:13:50 +0700

    fix: getting owner of thread

--- src/commands/solve.ts
+++ src/commands/solve.ts
@@ -14,14 +14,14 @@ module.exports = {
     async execute(interaction: CommandInteraction) {
         const thread = interaction.channel;
 
-        if (!thread || !thread.isThread() || !thread.parent || thread.archived) {
-            console.log("Something went very wrong. Command interaction wasn't sent from a valid thread, or the channel got removed.")
-            await interaction.reply("You can't call this command from here.")
-        } else if (thread.parent.name !== bug_channel) {
-            await interaction.reply(`This command is only accessible from threads created in the \`${bug_channel}\` channel`)
-        } else if (interaction.user.id !== thread.ownerId) {
+        if (!thread || !thread.isThread() || !thread.parent || thread.archived) { // if not thread
+            console.log("Something went very wrong. Command interaction wasn't sent from a valid thread, or the channel got removed.");
+            await interaction.reply("You can't call this command from here.");
+        } else if (thread.parent.name !== bug_channel) { // if not in bug reports channel
+            await interaction.reply(`This command is only accessible from threads created in the \`${bug_channel}\` channel`);
+        } else if (interaction.user.id !== (await thread.fetchStarterMessage()).author.id) { // if command issuer id != thread message starter
             if (interaction.member?.permissions !== SOLVE_PERMS) {
-                await interaction.reply("This command can only be issued by the creator of the issue or a moderator.")
+                await interaction.reply("This command can only be issued by the creator of the issue or a moderator.");
             }
         } else {
             await interaction.reply("Archiving thread...");
